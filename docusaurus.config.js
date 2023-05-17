3dcc9538abc43af73c79d3395b163964f87876d1
godfrey <tw.gary.com@gmail.com>
2023-05-18 02:25:26 +0800

    Change site name of nabber

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,7 +60,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'Godfrey.Dev',
+        title: 'Godfrey.Codes',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',

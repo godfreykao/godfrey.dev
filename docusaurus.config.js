b6262b553dadd3ddcc0209c87aa1626600580fa0
godfrey <tw.gary.com@gmail.com>
2023-05-18 02:22:05 +0800

    Change site name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Godfrey.Dev',
+  title: 'Godfrey.Codes',
   tagline: '關於筆記以及一些隨筆',
   favicon: 'img/favicon.ico',
 
